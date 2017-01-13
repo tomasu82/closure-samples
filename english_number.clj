0a378a436e281aa6574b18c0be9ee8c1acc04858
Thomas Kim <Thomas@Thomass-MacBook-Pro.local>
2017-01-13 00:09:11 -0500

    print interger in english

---
+++ english_number.clj
@@ -0,0 +1,27 @@
+(def small_numbers
+  ["one", "two", "three", "four", "five",
+  "six","seven","eight","nine","ten",
+  "eleven","twelve","thirteen","fourteen",
+  "fifteen","sixteen","seventeen","eighteen",
+  "nineteen"])
+
+(def large_numbers
+  ["twenty","thirty","fourty","fifty","sixty",
+  "seventy","eighty","ninety"])
+
+(defn english [number]
+  (cond
+    (and (< number 1000000000000) (>= number 1000000000))
+      (str (english (quot number 1000000000)) " billion " (english (rem number 1000000000)))
+    (and (< number 1000000000) (>= number 1000000))
+      (str (english (quot number 1000000)) " million " (english (rem number 1000000)))
+    (and (< number 1000000) (>= number 1000))
+      (str (english (quot number 1000)) " thousand " (english (rem number 1000)))
+    (and (< number 1000) (>= number 100))
+      (str (english (quot number 100)) " hundred " (english (rem number 100)))
+    (and (< number 100) (>= number 20))
+      (str (nth large_numbers (- (quot number 10) 2)) " " (english (rem number 10)) )
+    (and (< number 20) (> number 0))
+      (str (nth small_numbers (dec number)))
+    :else
+      ""))
